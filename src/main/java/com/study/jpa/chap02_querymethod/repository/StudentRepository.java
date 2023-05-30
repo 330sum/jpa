f2f4a497a293b68a01df2933874321e4088a1569
330sum <126743151+330sum@users.noreply.github.com>
2023-05-30 12:20:03 +0900

    :white_check_mark: # 3. JPQL

--- src/main/java/com/study/jpa/chap02_querymethod/repository/StudentRepository.java
+++ src/main/java/com/study/jpa/chap02_querymethod/repository/StudentRepository.java
@@ -3,6 +3,7 @@ package com.study.jpa.chap02_querymethod.repository;
 import com.study.jpa.chap02_querymethod.entity.Student;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -28,6 +29,31 @@ public interface StudentRepository extends JpaRepository<Student, String> {
     Student findNameWithSQL(@Param("nm")String name);
 //    @Query(value = "SELECT * FROM tbl_student WHERE stu_name = :name")
 //    Student findNameWithSQL(String name);
+
+
+    /* JPQL
+        SELECT 별칭 FROM 엔터티클래스명 as 별칭 WHERE 별칭.필드명 = ? (as 생략 가능)
+
+        native-sql : SELECT * FROM tbl_student WHERE stu_name = ?
+        jpql :       SELECT st FROM Student AS st WHERE st.name = ?  (as 생략 가능)
+    * */
+
+    // 도시 이름으로 학생 조회
+//    @Query(value = "SELECT * FROM tbl_student WHERE city = ?1", nativeQuery = true)
+    @Query("SELECT s FROM Student s WHERE s.city = ?1")
+    Student getByCityWithJPQL(String city);
+    // ?1 ?2 이런것 보다 @Param으로 지정해주는게 실무에서 더 좋은방법
+    // unique가 아니면 list로 받는게 맞음! 현재 테스트 용도로 이렇게 쓰는공!
+
+
+    @Query("SELECT st FROM Student st WHERE st.name LIKE %:nm%")
+    List<Student> searchByNamesWithJPQL(@Param("nm")String name);
+
+
+    /* JPQL로 수정 삭제 쿼리 쓰기 */
+    @Modifying /* 조회가 아닐 경우, @Modifying 무조건 붙여야 함 */
+    @Query("DELETE FROM Student s WHERE s.name = ?1")
+    void deleteByNameWithJPQL(String name);
 
 
 }
